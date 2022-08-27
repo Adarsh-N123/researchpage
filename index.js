d8d3aa37dd6908b131b50b1f5f8f1c8e6c1e39f8
adarsh_nidamanuri <99207348+Adarsh-N123@users.noreply.github.com>
2022-08-28 00:47:54 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,100 @@
+function func1(){
+    document.getElementById("loader").style.display="block";
+    document.getElementById("datacolumn").style.display="none";
+    document.getElementById("npage").style.display="none";
+    document.getElementById("npage1").style.display="none";
+    document.getElementById("npage2").style.display="none";
+    setTimeout(function(){
+        document.getElementById("loader").style.display="none";
+        document.getElementById("npage").style.display="block";
+        document.getElementById("npage1").style.display="none";
+        document.getElementById("npage2").style.display="none";
+        document.getElementById("datacolumn").style.display="none";
+            
+    },1000)
+    
+}
+function func2(){
+    document.getElementById("loader").style.display="block";
+    document.getElementById("datacolumn").style.display="none";
+    document.getElementById("npage").style.display="none";
+    document.getElementById("npage1").style.display="none";
+    document.getElementById("npage2").style.display="none";
+    setTimeout(function(){
+        document.getElementById("loader").style.display="none";
+        document.getElementById("npage").style.display="none";
+        document.getElementById("npage1").style.display="block";
+        document.getElementById("npage2").style.display="none";
+        document.getElementById("datacolumn").style.display="none";
+        
+    },1000)
+    
+
+}
+function func3(){
+    document.getElementById("loader").style.display="block";
+    document.getElementById("datacolumn").style.display="none";
+    document.getElementById("npage").style.display="none";
+    document.getElementById("npage1").style.display="none";
+    document.getElementById("npage2").style.display="none";
+    setTimeout(function(){
+        document.getElementById("loader").style.display="none";
+        document.getElementById("npage").style.display="none";
+        document.getElementById("npage1").style.display="none";
+        document.getElementById("npage2").style.display="block";
+        document.getElementById("datacolumn").style.display="none";
+        
+    },1000)
+    
+}
+function func4(){
+    document.getElementById("loader").style.display="block";
+    document.getElementById("datacolumn").style.display="none";
+    document.getElementById("npage").style.display="none";
+    document.getElementById("npage1").style.display="none";
+    document.getElementById("npage2").style.display="none";
+    setTimeout(function(){
+        document.getElementById("loader").style.display="none";
+        document.getElementById("npage").style.display="none";
+        document.getElementById("npage1").style.display="none";
+        document.getElementById("npage2").style.display="none";
+        document.getElementById("datacolumn").style.display="block";
+        
+    },1000)
+    
+
+}
+function func5(){
+    document.getElementById("loader").style.display="block";
+    document.getElementById("datacolumn").style.display="none";
+    document.getElementById("npage").style.display="none";
+    document.getElementById("npage1").style.display="none";
+    document.getElementById("npage2").style.display="none";
+    setTimeout(function(){
+        document.getElementById("loader").style.display="none";
+        document.getElementById("npage").style.display="none";
+        document.getElementById("npage1").style.display="none";
+        document.getElementById("npage2").style.display="none";
+        document.getElementById("datacolumn").style.display="block";
+        
+    },1000)
+    
+
+}
+function func6(){
+    document.getElementById("loader").style.display="block";
+    document.getElementById("datacolumn").style.display="none";
+    document.getElementById("npage").style.display="none";
+    document.getElementById("npage1").style.display="none";
+    document.getElementById("npage2").style.display="none";
+    setTimeout(function(){
+        document.getElementById("loader").style.display="none";
+        document.getElementById("npage").style.display="none";
+        document.getElementById("npage1").style.display="none";
+        document.getElementById("npage2").style.display="none";
+        document.getElementById("datacolumn").style.display="block";
+        
+    },1000)
+    
+
+}
